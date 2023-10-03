d6a7a9933c9ca4211ad0c0de66c1bd306805feac
Valeria <valeria.s@outlook.com.br>
2023-10-02 21:25:45 -0300

    teste

--- exclude_unwanted_files.js
+++ exclude_unwanted_files.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.readFile('/abaplint.json', 'utf8', (err, data) => {
+fs.readFile('\abaplint.json', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
